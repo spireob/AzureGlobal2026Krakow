418c27ae9774bc6b22f33cd086482cec0004bf01
spireob <39996546+spireob@users.noreply.github.com>
2026-04-16 19:31:35 +0200

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">4.1.0"
+    }
+  }
+}
+provider "azurerm" {
+  subscription_id = "4c569ea4-8bfc-4063-9557-390e4b28a153"
+  features {}
+}
+
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-user7" #change here
+    storage_account_name = "rguser7sta01" #change here
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
