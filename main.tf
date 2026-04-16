409bdb6ab99c146e093485c4f5dc08bc28c7d05e
spireob <39996546+spireob@users.noreply.github.com>
2026-04-16 20:15:05 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,11 @@ locals {
     acr_name = "rguser7acr"
     keyvault_name = "rg-user7-kv"
     sql_server_name = "rg-user7-sql"
+    application_insights_name = "rg-user7-applins"
+    log_analytics_name = "rg-user7-la"
+    app_service_plan_name = "rg-user7-asp"
+    app_service_name = "rg-user7-aps"
+
 }
 resource "azurerm_resource_group" "rg" {
   name = "${local.resource_group}"
@@ -70,4 +75,47 @@ module "mssql_server" {
   sql_server_name = local.sql_server_name
   sql_server_version = "12.0"
 
+}
+
+
+module "application_insights" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=application_insights/v1.0.0"
+  # also any inputs for the module (see below)
+  application_insights_name = local.application_insights_name
+  log_analytics_name = local.log_analytics_name
+  resource_group = {
+    name = "${local.resource_group}"
+    location = "${local.location}"
+  }
+}
+
+
+module "service_plan" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=service_plan/v2.0.0"
+  # also any inputs for the module (see below)
+  app_service_plan_name = local.app_service_plan_name
+  resource_group = {
+    name = "${local.resource_group}"
+    location = "${local.location}"
+  }
+  sku_name = "B1"
+  tags = local.tags 
+
+}
+
+
+module "app_service" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=app_service/v1.0.0"
+  # also any inputs for the module (see below)
+  app_service_name = local.app_service_name
+  app_service_plan_id = module.service_plan.app_service_plan.id
+  app_settings = {
+
+  }
+  identity_client_id = module.umi.managed_identity_client_id
+  identity_id = module.umi.managed_identity_id
+  resource_group = {
+    name = "${local.resource_group}"
+    location = "${local.location}"
+  }
 }
